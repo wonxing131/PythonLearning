24d3b4437b5a373125f2556b782fe9975b3e141a
dingtongchuan <dingtongchuan@qianxin.com>
2020-01-09 10:17:45 +0800

    模块，高级函数编程

--- FunctionallProgramming.py
+++ FunctionallProgramming.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import functools
 
 
 def add(x, y, f):
@@ -40,7 +41,6 @@ def not_empty(s):
 n = list(filter(not_empty, ['A', '', 'B', None, 'C', '  ']))
 print(n)
 
-
 print(sorted([36, 5, -12, 9, -21]))
 print(sorted([36, 5, -12, 9, -21], key=abs))
 print(sorted(['bob', 'about', 'Zoo', 'Credit']))
@@ -54,6 +54,7 @@ def lazy_sum(*args):
         for n in args:
             ax = ax + n
         return ax
+
     return sum
 
 
@@ -65,8 +66,10 @@ print(f())
 def count():
     def f(j):
         def g():
-            return j*j
+            return j * j
+
         return g
+
     fs = []
     for i in range(1, 4):
         fs.append(f(i))  # f(i)立刻被执行，因此i的当前值被传入f()
@@ -76,6 +79,53 @@ def count():
 f1, f2, f3 = count()
 print(f1(), f2(), f3())
 
-
 print(list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 
+
+def now():
+    print('2015-3-25')
+
+
+f = now
+f()
+print(now.__name__)
+print(f.__name__)
+
+
+# Decorator 假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，
+# 这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+# 本质上，decorator就是一个返回函数的高阶函数。所以，我们要定义一个能打印日志的decorator，可以定义如下：
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+
+    return wrapper
+
+
+@log
+def now():
+    print('2015-3-25')
+
+
+now()
+
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@log('execute')
+def now():
+    print('2015-3-25')
+
+
+now()
+int2 = functools.partial(int, base=2)
+print(int2('1000000'))
+print(int2('1010101'))
